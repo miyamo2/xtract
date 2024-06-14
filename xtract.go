b01fe10369f845b3e96d4a149b151bd8f2efc48e
miyamo2 <miyamo2@outlook.com>
2024-06-14 13:06:12 +0900

    feat: impl

---
+++ xtract.go
@@ -0,0 +1,121 @@
+package xtract
+
+// compatibility checks
+var (
+	_ Extractor[int, string] = (*SliceExtractor[string])(nil)
+	_ Extractor[string, int] = (*MapExtractor[string, int])(nil)
+)
+
+// Extractor provides features to extract values from a collection.
+type Extractor[K comparable, V any] interface {
+	// ByValue filters the values of the collection by their values.
+	ByValue(condition func(V) bool) func(yield func(V) bool)
+	// ByKey filters the values of the collection by their keys.
+	ByKey(condition func(K) bool) func(yield func(V) bool)
+	// ByKeyAndValue filters the values of the collection by their keys and values.
+	ByKeyAndValue(condition func(K, V) bool) func(yield func(V) bool)
+}
+
+// SliceExtractor is implementation of Extractor for slice.
+type SliceExtractor[T any] struct {
+	s []T
+}
+
+// ByValue See: Extractor.ByValue
+func (x *SliceExtractor[T]) ByValue(condition func(T) bool) func(yield func(T) bool) {
+	return func(yield func(T) bool) {
+		for _, v := range x.s {
+			if condition(v) {
+				if !yield(v) {
+					break
+				}
+			}
+		}
+	}
+}
+
+// ByKey See: Extractor.ByKey
+func (x *SliceExtractor[T]) ByKey(condition func(int) bool) func(yield func(T) bool) {
+	return func(yield func(T) bool) {
+		for i, v := range x.s {
+			if condition(i) {
+				if !yield(v) {
+					break
+				}
+			}
+		}
+	}
+}
+
+// ByKeyAndValue See: Extractor.ByKeyAndValue
+func (x *SliceExtractor[T]) ByKeyAndValue(condition func(int, T) bool) func(yield func(T) bool) {
+	return func(yield func(T) bool) {
+		for i, v := range x.s {
+			if condition(i, v) {
+				if !yield(v) {
+					break
+				}
+			}
+		}
+	}
+}
+
+// FromSlice returns Extractor for a slice.
+func FromSlice[T any](s []T) Extractor[int, T] {
+	cp := make([]T, 0, len(s))
+	cp = append(cp, s...)
+	return &SliceExtractor[T]{s: cp}
+}
+
+// MapExtractor is implementation of Extractor for map.
+type MapExtractor[T comparable, U any] struct {
+	m map[T]U
+}
+
+// ByValue See: Extractor.ByValue
+func (x MapExtractor[K, V]) ByValue(condition func(V) bool) func(yield func(V) bool) {
+	return func(yield func(V) bool) {
+		for _, v := range x.m {
+			if condition(v) {
+				if !yield(v) {
+					break
+				}
+			}
+		}
+	}
+}
+
+// ByKey See: Extractor.ByKey
+func (x MapExtractor[K, V]) ByKey(condition func(K) bool) func(yield func(V) bool) {
+	return func(yield func(V) bool) {
+		for i, v := range x.m {
+			if condition(i) {
+				if !yield(v) {
+					break
+				}
+			}
+		}
+	}
+}
+
+// ByKeyAndValue See: Extractor.ByKeyAndValue
+func (x MapExtractor[K, V]) ByKeyAndValue(condition func(K, V) bool) func(yield func(V) bool) {
+	return func(yield func(V) bool) {
+		for k, v := range x.m {
+			if condition(k, v) {
+				if !yield(v) {
+					break
+				}
+			}
+		}
+	}
+}
+
+// FromMap returns Extractor for a map.
+func FromMap[K comparable, V any](m map[K]V) Extractor[K, V] {
+	cp := make(map[K]V)
+	for k, v := range m {
+		cp[k] = v
+	}
+	return &MapExtractor[K, V]{m: cp}
+}
---
+++ xtract_example_test.go
@@ -0,0 +1,49 @@
+package xtract_test
+
+import (
+	"fmt"
+	"github.com/miyamo2/xtract"
+	"strings"
+)
+
+func ExampleSliceExtractor_ByValue() {
+	s := []string{"gopher", "iterator", "range over func"}
+	for v := range xtract.FromSlice(s).ByValue(func(v string) bool { return len(v) < 9 }) {
+		fmt.Println(v)
+	}
+	// Output: gopher
+	//iterator
+}
+
+func ExampleSliceExtractor_ByKey() {
+	s := []string{"gopher", "iterator", "range over func"}
+	for v := range xtract.FromSlice(s).ByKey(func(i int) bool { return i < 2 }) {
+		fmt.Println(v)
+	}
+	// Output: gopher
+	//iterator
+}
+
+func ExampleSliceExtractor_ByKeyAndValue() {
+	s := []string{"gopher", "iterator", "range over func"}
+	for v := range xtract.FromSlice(s).ByKeyAndValue(func(i int, v string) bool { return i > 1 && len(v) > 6 }) {
+		fmt.Println(v)
+	}
+	// Output: range over func
+}
+
+func ExampleMapExtractor_ByValue() {
+	m := map[string]string{"language": "gopher", "design pattern": "iterator", "implementation": "range over func"}
+	for v := range xtract.FromMap(m).ByValue(func(v string) bool { return len(v) < 8 }) {
+		fmt.Println(v)
+	}
+	// Output: gopher
+}
+
+func ExampleMapExtractor_ByKey() {
+	m := map[string]string{"language": "gopher", "design pattern": "iterator", "implementation": "range over func"}
+	for v := range xtract.FromMap(m).ByKey(func(k string) bool { return strings.Contains(k, " ") }) {
+		fmt.Println(v)
+	}
+	// Output: iterator
+}
